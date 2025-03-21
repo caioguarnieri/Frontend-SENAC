7acba7e5827e5b67fe8d6e96afedaabbcbfefe91
caioguarnieri <caioguarniele@hotmail.com>
2025-03-20 22:23:58 -0300

    Learning DO, WHILE FOR

--- Javascript Front-End/script.js
+++ Javascript Front-End/script.js
@@ -6,8 +6,7 @@ function somar() {
   let numero1 = Number(n1.value);
   let numero2 = Number(n2.value);
   let somatoria = numero1 + numero2;
-
-  resposta1.innerText = `A Soma entre ${numero1} e ${numero2} é ${somatoria} :D`;
+  resposta1.innerText = `A Soma entre ${numero1} e ${numero2} é ${somatoria}`;
 }
 
 let fundo0 = window.document.getElementById("01");
@@ -110,9 +109,79 @@ function mudarCorIn8() {
   fundo8.style.backgroundColor = "#F08080";
 }
 
-function mudarCorOut8() {
+function mudarCorOut8(){
   fundo8.style.backgroundColor = "white";
 }
+function mudarCorIn9() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut9(){
+  fundo8.style.backgroundColor = "white";
+}
+function mudarCorIn10() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut10(){
+  fundo8.style.backgroundColor = "white";
+}
+function mudarCorIn11() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut11(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn12() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut12(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn13() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut13(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn14() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut14(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn15() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut15(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn16() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut16(){
+  fundo8.style.backgroundColor = "white";
+}
+
+function mudarCorIn17() {
+  fundo8.style.backgroundColor = "#F08080";
+}
+
+function mudarCorOut17(){
+  fundo8.style.backgroundColor = "white";
+}
+
 
 function caixa() {
   let produto = window.document.getElementById("produto1");
@@ -191,103 +260,163 @@ function menorDeTres() {
   }
 }
 
-function minutosValor(){
-    let minutos = Number(window.document.getElementById('minutos').value);
-    let franquia = 100;
-    let overFranquia = minutos - franquia;
-    let resultadoFranquia = window.document.getElementById('valorAPagar');
-    totalFranquia = '';
-    valorFranquia = 50;
-    totalGeral = '';
+function minutosValor() {
+  let minutos = Number(window.document.getElementById("minutos").value);
+  let franquia = 100;
+  let overFranquia = minutos - franquia;
+  let resultadoFranquia = window.document.getElementById("valorAPagar");
+  totalFranquia = "";
+  valorFranquia = 50;
+  totalGeral = "";
 
-
-    if (minutos > franquia){
-        totalFranquia = overFranquia * 2;
-        totalGeral = valorFranquia + totalFranquia;
-        resultadoFranquia.innerText = `O valor a pagar é ${totalGeral} reais` 
-    }else{
-        resultadoFranquia.innerText = `O valor a pagar é ${valorFranquia} reais`
-    }
+  if (minutos > franquia) {
+    totalFranquia = overFranquia * 2;
+    totalGeral = valorFranquia + totalFranquia;
+    resultadoFranquia.innerText = `O valor a pagar é ${totalGeral} reais`;
+  } else {
+    resultadoFranquia.innerText = `O valor a pagar é ${valorFranquia} reais`;
+  }
 }
 
-function glicose(){
-    let glicoseValue = Number(window.document.getElementById('glicoseValue').value);
-    let normal = 100;
-    let elevado = 140;
-    let resultadoGlicose = window.document.getElementById('glicoseTotal');
+function glicose() {
+  let glicoseValue = Number(
+    window.document.getElementById("glicoseValue").value
+  );
+  let normal = 100;
+  let elevado = 140;
+  let resultadoGlicose = window.document.getElementById("glicoseTotal");
 
-    if (glicoseValue <= normal){
-        resultadoGlicose.innerText = `Resultado: Normal`;
-        resultadoGlicose.style.backgroundColor = '#90ee90';
-} else if (glicoseValue <= elevado ){
+  if (glicoseValue <= normal) {
+    resultadoGlicose.innerText = `Resultado: Normal`;
+    resultadoGlicose.style.backgroundColor = "#90ee90";
+  } else if (glicoseValue <= elevado) {
     resultadoGlicose.innerText = `Resultado: Elevado`;
-    resultadoGlicose.style.backgroundColor = '#ffff00';
-   }  else if (glicoseValue > 140 ){
+    resultadoGlicose.style.backgroundColor = "#ffff00";
+  } else if (glicoseValue > 140) {
     resultadoGlicose.innerText = `Resultado: Diabetes`;
-    resultadoGlicose.style.backgroundColor = '#f1807e';
-   }
+    resultadoGlicose.style.backgroundColor = "#f1807e";
+  }
 }
 
 function maiorDistancia() {
-    let dis1 = Number(window.document.getElementById("distancia1").value);
-    let dis2 = Number(window.document.getElementById("distancia2").value);
-    let dis3 = Number(window.document.getElementById("distancia3").value);
-    /// ESSE SERIA O MAIS CURTO
-    ///   let menordeTodos = Math.min(numero01, numero02, numero03);
-    let maiorDeTodos = " ";
-    let resultadoDistancia = window.document.getElementById("maiorDistanciaFinal");
+  let dis1 = Number(window.document.getElementById("distancia1").value);
+  let dis2 = Number(window.document.getElementById("distancia2").value);
+  let dis3 = Number(window.document.getElementById("distancia3").value);
+  /// ESSE SERIA O MAIS CURTO
+  ///   let menordeTodos = Math.min(numero01, numero02, numero03);
+  let maiorDeTodos = " ";
+  let resultadoDistancia = window.document.getElementById(
+    "maiorDistanciaFinal"
+  );
+
+  if (dis1 >= dis2 && dis3) {
+    let maiorDeTodos = dis1;
+    resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
+  }
+  if (dis2 >= dis1 && dis3) {
+    let maiorDeTodos = dis2;
+    resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
+  }
+  if (dis3 >= dis1 && dis2) {
+    let maiorDeTodos = dis3;
+    resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
+  }
+}
+
+function converterTemp() {
+  let selecaoTemperatura = Number(
+    window.document.getElementById("selecao").value
+  );
+  let userTemp = Number(window.document.getElementById("temperatura").value);
+
+  let cTof = userTemp * 1.8 + 32;
+  let fToC = (userTemp - 32) / 1.8;
+  let tempResult = window.document.getElementById("tempFinal");
+  cTof = cTof.toFixed(2);
+  fToC = fToC.toFixed(2);
+
+  switch (selecaoTemperatura) {
+    case 0:
+      tempResult.innerText = `A temperatudo em Fahrenheit é ${cTof}`;
+      break;
+    case 1:
+      tempResult.innerText = `A temperatudo em Celcius é ${fToC}`;
+      break;
+  }
+}
+
+function caixaCodigo() {
+  let productId = Number(window.document.getElementById("produtoCodigo").value);
+  let unitPrice = "";
+  let productUnits = Number(
+    window.document.getElementById("produtoQuantidade").value
+  );
+  let valorUnidade = window.document.getElementById("valorUnitario");
+  let valorFinal = window.document.getElementById("valorFinal");
+
+  switch (productId) {
+    case 0:
+      unitPrice = 5;
+      valorUnidade.innerText = `O valor unitário é ${unitPrice}`;
+      valorFinal.innerText = `O valor final é ${unitPrice * productUnits}`;
+      break;
+    case 1:
+      unitPrice = 10;
+      valorUnidade.innerText = `O valor unitário é ${unitPrice}`;
+      valorFinal.innerText = `O valor final é ${unitPrice * productUnits}`;
+      break;
+  }
+}
+
+function sobeOuDesce() {
+  let num1 = 0;
+  let num2 = 0;
+
+  do {
+    let num1 = Number(window.prompt("Digite seu 1º número aqui"));
+    let num2 = Number(window.prompt("Digite seu 2º número aqui"));
+    let result = window.document.getElementById("oNumeroE");
+    total = num1 - num2;
+    if (total < 0) {
+      alert(`CRESCENTE`);
+    } else if (total > 0) {
+      alert(`DECRESCENTE`);
+    } else {
+      alert("Números iguais!");
+    }
+  } while (num1 != num2);
+}
+
+function iniciarSenha() {
+  let userSenha = window.prompt("DEFINA SUA SENHA AQUI");
+  let tentativa;
   
-    if (dis1 >= dis2 && dis3) {
-      let maiorDeTodos = dis1;
-      resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
+  do {
+    tentativa = prompt("Tente acertar a Senha");
+    
+    if (tentativa != userSenha) {
+      alert("Senha Incorreta");
+    } else {
+      alert(`Senha Correta!!!`);
     }
-    if (dis2 >= dis1 && dis3) {
-      let maiorDeTodos = dis2;
-      resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
+
+  } while(tentativa != userSenha);
+}
+
+function validarNotas(){
+    let nota01 = window.prompt('Insira a primeira nota');
+    let nota02 = window.prompt('Insira a 2ª nota');
+    let notaValida1 ='';
+    let notaValida2 = '';
+    let resultadoNota = validarNota1 + validarNota1 /2;
+   
+    while(nota01 >= 10  ){
+        notaValida1 = nota01;
     }
-    if (dis3 >= dis1 && dis2) {
-      let maiorDeTodos = dis3;
-      resultadoDistancia.innerText = `A maior distância é : ${maiorDeTodos}`;
-    }
-  }
 
-  function converterTemp(){
-      let selecaoTemperatura = Number(window.document.getElementById('selecao').value);
-      let userTemp = Number(window.document.getElementById('temperatura').value);
-      
-      let cTof = userTemp * 1.8 + 32 ;
-      let fToC = (userTemp - 32) / 1.8;
-      let tempResult = window.document.getElementById('tempFinal');
-      cTof = cTof.toFixed(2);
-      fToC = fToC.toFixed(2);
-      
-      
-      switch (selecaoTemperatura){
-          case 0:
-              tempResult.innerText = `A temperatudo em Fahrenheit é ${cTof}`;
-              break
-            case 1: 
-            tempResult.innerText = `A temperatudo em Celcius é ${fToC}`;
-              break
-      }
+    while(nota02 >= 10  ){
+      notaValida2 = nota02;
   }
-
-  function caixaCodigo() {
-      let productId = Number(window.document.getElementById('produtoCodigo').value);
-      let unitPrice = '';
-      let totalFinal = produtoQuantidade * unitPrice;
-      let valorUnidade = window.document.getElementById('valorUnitario');
-      let valorFinal = window.document.getElementById('valorFinal');
-
-      switch (productId){
-        case 0:
-            unitPrice = 2
-            valorUnidade.innerText = `O valor unitário é ${unitPrice}`;
-            valorFinal.innerText = `O valor final é ${totalFinal}`;
-            break
-          case 1:
-          unitPrice = 3
-          valorUnidade.innerText = `O valor unitário é ${unitPrice}`;
-          valorFinal.innerText = `O valor final é ${totalFinal}`;
-          break
-  }
+    alert(`A nota 1 é ${nota01} e a nota 2 é ${nota02}Sua média é ${resultadoNota}`)
+  
+}
